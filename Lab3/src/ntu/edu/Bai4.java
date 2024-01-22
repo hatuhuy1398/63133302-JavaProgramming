3a437738a9f63beeb57f881a3f7544066cf4e1fa
Ha Tu Huy <152080712+hatuhuy1398@users.noreply.github.com>
2024-01-22 23:09:52 +0700

    Lab4
    edit lab3 bai 3

--- Lab3/src/ntu/edu/Bai4.java
+++ Lab3/src/ntu/edu/Bai4.java
@@ -35,30 +35,53 @@ public class Bai4 {
                System.out.println();
         }
 		System.out.print("\nThong tin cua sinh vien sau khi sap xep:\n");
-		double min = diem[0];
-		
-		for (int i = 1; i < n+1; ++i) {
-			
-	           if(min<diem[i]){
-	        System.out.println("Sinh vien " + (i + 1) + ":");
-            System.out.println("Ho ten: " + hoTen[i-1]);
-            System.out.println("Điem: " + diem[i-1]);
-            System.out.println("Hoc luc: " + hocLuc(diem[i-1]));
+		sapXepSinhVien(hoTen, diem, n);
+		for (int i = 0; i < n; ++i) {
+	           
+     	   System.out.println("Sinh vien " + (i + 1) + ":");
+            System.out.println("Ho ten: " + hoTen[i]);
+            System.out.println("Điem: " + diem[i]);
+            System.out.println("Hoc luc: " + hocLuc(diem[i]));
             System.out.println();
-            min = diem[i];
-   
-	        }     
 		}
-		
 	}
-	private static String hocLuc(double diem) {
-        if (diem >= 8.0) {
+	static void sapXepSinhVien(String[] hoTen, double[] diem, int n)
+	{
+		boolean ok;
+
+        for (int i = 0; i < n - 1; ++i) {
+            ok = false;
+
+            for (int j = 0; j < n - i - 1; ++j) {
+                if (diem[j] > diem[j + 1]) {
+                    double temp = diem[j];
+                    diem[j] = diem[j + 1];
+                    diem[j + 1] = temp;
+                    ok = true;
+                    
+                    String tempString = hoTen[j];
+                    hoTen[j] = hoTen[j + 1];
+                    hoTen[j + 1] = tempString;
+                    ok = true;
+                }
+            }
+
+          
+            if (!ok) {
+                break;
+            }
+        }
+	}	private static String hocLuc(double diem) {
+        if (diem >= 9.0) {
+            return "Xuat Sac";
+        } else if (diem >= 7.5) {
             return "Gioi";
         } else if (diem >= 6.5) {
             return "Kha";
-        } else if (diem >= 5.0) {
-            return "Trung binh";
-        } else {
+        }else if (diem >= 5.0) {
+            return "Trung Binh";
+        }
+        else {
             return "Dot";
         }
     }
