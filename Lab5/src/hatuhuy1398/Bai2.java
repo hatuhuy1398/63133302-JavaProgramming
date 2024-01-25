b4dd8d3d37ea6ed3b1718d452789ea887562f76e
Ha Tu Huy <152080712+hatuhuy1398@users.noreply.github.com>
2024-01-25 13:52:24 +0700

    lab4 bai 2
    update code

--- Lab5/src/hatuhuy1398/Bai2.java
+++ Lab5/src/hatuhuy1398/Bai2.java
@@ -1,48 +1,14 @@
 package hatuhuy1398;
+
+
 import java.util.*;
 
 
 public class Bai2 {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		ArrayList<String> listName = new ArrayList<>();
-	   
-        while(true) {
-        	Menu();
-        	System.out.print("Nhap vao lua chon 1->6: ");
-            int choice = scanner.nextInt();
-           
-            scanner.nextLine();
-           
-            switch (choice) {
-                case 1:
-                    Nhap(listName, scanner);;
-                    break;
-                case 2:
-                    Xuat(listName);
-                    break;
-                case 3:
-                    sapXep(listName);
-                    break;
-                case 4:
-                    ngauNhien(listName);
-                    break;
-                case 5:
-                	xoa(listName,scanner);
-                	break;
-                case 6:
-                    System.out.println("Ứng dụng đã thoát.");
-                    break;
-                default:
-                    System.out.println("Lựa chọn không hợp lệ. Hãy chọn lại.");
-            }
-        }
-		
-		
-	}
-	 static void Menu() {
+	
+	private static void Menu() {
 		System.out.println("****************************************");
-		System.out.println("*************Chon chuc nang***********");
+		System.out.println("*************Chon chuc nang*************");
 		System.out.println("*************1.Nhap*********************");
 		System.out.println("*************3.Sap xep******************");
 		System.out.println("*************2.Xuat*********************");
@@ -51,38 +17,39 @@ public class Bai2 {
 		System.out.println("***********6.Thoat chuong trinh*********");
 		System.out.println("****************************************");
 	}
-	 static void Nhap(ArrayList<String> listName, Scanner scanner) {
-	
+	private static void Nhap(ArrayList<String> listName) {
+		Scanner scanner = new Scanner(System.in);
             System.out.print("Nhap vao ho ten: ");
             String hoTen = scanner.nextLine();
             listName.add(hoTen);
             
 	}
-	static void Xuat(ArrayList<String> listName) {
+	private static void Xuat(ArrayList<String> listName) {
 		
-        System.out.print("\nDanh sach vao ho ten: \n");
+        System.out.print("\nDanh sach ho ten: \n");
         for(String hoTen:listName) {
         	System.out.printf("%s\n",hoTen);
         }
         
 	}
-	static void sapXep(ArrayList<String> listName) {
+	private static void sapXep(ArrayList<String> listName) {
 		Collections.sort(listName);
 		Collections.reverse(listName);
-		 System.out.print("\nDanh sach vao ho ten sap xep: \n");
+		 System.out.print("\nDanh sach ho ten da sap xep: \n");
 		for(String hoTen:listName) {
 			
-        	System.out.println(hoTen);
+        	System.out.printf("%s",hoTen);
         }
 	}
-	static void ngauNhien(ArrayList<String> listName) {
+	private static void ngauNhien(ArrayList<String> listName) {
 		Collections.shuffle(listName);
 		 System.out.print("\nDanh sach vao ho ten ngau nhien: \n");
 		for(String hoTen:listName) {
         	System.out.printf("\n%s\n",hoTen);
         }
 	}
-	static void xoa(ArrayList<String> listName, Scanner scanner) {
+	private static void xoa(ArrayList<String> listName) {
+		Scanner scanner = new Scanner(System.in);
 		System.out.print("Nhap vao ten can xoa: ");
 	    String hoTenCanXoa = scanner.nextLine();
 	    Iterator<String> iterator = listName.iterator();
@@ -101,6 +68,42 @@ public class Bai2 {
 	    if (!found) {
 	        System.out.println("Khong tim thay ten can xoa trong danh sach!!");
 	    }
+	}
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<String> listName = new ArrayList<String>();
+		int choice ;
+        do {
+        	Menu();
+        	
+        	System.out.print("Nhap vao lua chon 1->6: ");
+        	choice = scanner.nextInt();
+           
+            switch (choice) {
+                case 1:
+                    Nhap(listName);
+                    break;
+                case 2:
+                    Xuat(listName);
+                    break;
+                case 3:
+                    sapXep(listName);
+                    break;
+                case 4:
+                    ngauNhien(listName);
+                    break;
+                case 5:
+                	xoa(listName);
+                	break;
+                case 6:
+                    System.out.println("Ứng dụng đã thoát.");
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ. Hãy chọn lại.");
+            }
+        }while(choice != 6);
+		
+		
 	}
 
 }
